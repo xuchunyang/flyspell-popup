26762d4278b61ff26c0d7e9d63c74e4d02124963
Chunyang Xu <xuchunyang56@gmail.com>
2015-06-28 21:06:40 +0800

    Add documentation

---
+++ README.md
@@ -0,0 +1,11 @@
+# flyspell-popup
+![](./images/screenshot-1.png?raw=true)
+
+Correct the misspelled word with flyspell in popup menu.
+
+## Usage
+To use, put your cursor on or after the misspelled word and call
+`flyspell-popup-correct`. You can of course bind it to a key as well by adding
+this to your Emacs initialization file, e.g. ~/.emacs.d/init.el:
+
+    (define-key flyspell-mode-map (kbd "C-;") #'flyspell-popup-correct)
--- flyspell-popup.el
+++ flyspell-popup.el
@@ -23,8 +23,17 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
 ;;
+;; Correct the misspelled word with `flyspell' in popup menu.
+;;
+;; Usage
+;; =====
+;;
+;; To use, put your cursor on or after the misspelled word and call
+;; `flyspell-popup-correct'. You can of course bind it to a key as well by adding
+;; this to your Emacs initialization file, e.g. ~/.emacs.d/init.el:
+;;
+;;   (define-key flyspell-mode-map (kbd "C-;") #'flyspell-popup-correct)
 
 ;;; Code:
 
